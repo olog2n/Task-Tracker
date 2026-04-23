2c3937f6bbf5f0018af365b584c29f60250e93e7
Valerii Loshchenkov <klarkisek@gmail.com>
2026-04-23 19:11:54 +0300

    Added docker

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,8 @@
 <script setup></script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <h1 class="text-3xl font-bold text-blue-500">Привет Tailwind!</h1>
 </template>
+
 
 <style scoped></style>
