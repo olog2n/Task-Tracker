b36c8fb7c1c4a44f78c16b633dae91597c58ad98
BASYME <dan.eremok@gmail.com>
2026-05-24 12:45:27 +0300

    fix: text in ProfileView

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -71,7 +71,7 @@ const onSubmit = handleSubmit(async (values) => {
 <template>
   <!-- 🚫 НЕТ <AppLayout>! Vue Router сам вставит этот блок внутрь лейаута -->
   <div class="max-w-2xl mx-auto p-6 bg-white rounded-xl shadow-sm">
-    <h1 class="text-2xl font-bold mb-6 text-gray-800">Мой профиль</h1>
+    <h1 class="text-2xl font-bold mb-6 text-gray-800 text-center">Мой профиль</h1>
 
     <form @submit.prevent="onSubmit" class="space-y-6">
 <div class="inline-flex flex-col items-center">
